e314e92b3fcfc419efbb63703ad59696cb32adbe
Bruuon <105570809@qq.com>
2024-11-07 01:38:32 +0800

    resize finished

--- src/resize/impl.cc
+++ src/resize/impl.cc
@@ -14,43 +14,42 @@ cv::Mat my_resize(const cv::Mat &input, float scale)
    * 通过条件：
    * 运行测试点，你的结果跟答案长的差不多就行。
    */
-   int new_rows = input.rows * scale, new_cols = input.cols * scale;
+  int new_rows = input.rows * scale, new_cols = input.cols * scale;
   // IMPLEMENT YOUR CODE HERE
-  int h = input.rows, w = input.cols, c = input.channels();
+  int input_rows = input.rows, input_cols = input.cols, channels = input.channels();
 
   float x0, y0, dx, dy;
   int x1, x2, x3, x4, y1, y2, y3, y4;
-  new_rows=input.rows;new_cols=input.cols;
-  cv::Mat_<cv::Vec3d> output=cv::Mat(1000, 1000, input.type());
-  std::cout << input.rows << ' ' << input.cols << std::endl;
-  std::cout << new_rows << ' ' << new_cols << ' ' << c << std::endl;
-  for (int row = 0; row < new_rows; ++row) {
-    for (int col = 0; col < new_cols; ++col) {
-      // for (int row = 0; row < std::min(input.rows, output.rows); ++row) {
-      //   for (int col = 0; col < std::min(input.cols, output.cols); ++col) {
+  
+  CV_Assert(input.depth() == CV_8U);
+  cv::Mat output = cv::Mat::zeros(new_rows, new_cols, input.type());
 
-      // // center point
-      // x0 = col / scale; y0 = row / scale;
-      // // top-left
-      // x1 = static_cast<int>(x0) % w; y1 = static_cast<int>(y0) % h;
-      // // top-right
-      // x2 = (x1 + 1) % w; y2 = y1;
-      // // bottom-left
-      // x3 = x1; y3 = (y1 + 1) % h;
-      // // bottom-right
-      // x4 = (x1 + 1) % w; y4 = (y1 + 1) % h;
+  cv::Vec3b P1, P2, P3, P4, *Q;
+  
+  for (int i = 0; i < new_rows; ++i) {
+    for (int j = 0; j < new_cols; ++j) {
+      // center point
+      x0 = j / scale; y0 = i / scale;
+      // top-left
+      x1 = static_cast<int>(x0) % input_cols; y1 = static_cast<int>(y0) % input_rows;
+      // top-right
+      x2 = (x1 + 1) % input_cols; y2 = y1;
+      // bottom-left
+      x3 = x1; y3 = (y1 + 1) % input_rows;
+      // bottom-right
+      x4 = (x1 + 1) % input_cols; y4 = (y1 + 1) % input_rows;
 
-      // dx = x0 - x1; dy = y0 - y1;
+      dx = x0 - x1; dy = y0 - y1;
 
-      // const cv::Vec3d &P1 = input.at<cv::Vec3d>(y1, x1);
-      // const cv::Vec3d &P2 = input.at<cv::Vec3d>(y2, x2);
-      // const cv::Vec3d &P3 = input.at<cv::Vec3d>(y3, x3);
-      // const cv::Vec3d &P4 = input.at<cv::Vec3d>(y4, x4);
+      P1 = input.at<cv::Vec3b>(y1, x1);
+      P2 = input.at<cv::Vec3b>(y2, x2);
+      P3 = input.at<cv::Vec3b>(y3, x3);
+      P4 = input.at<cv::Vec3b>(y4, x4);
 
-      cv::Vec3d &Q = output.at<cv::Vec3d>(row, col);
-      for (int i = 0; i < c; ++i) {
-        // Q[i] = P1[i] * (1 - dx) * (1 - dy) + P2[i] * dx * (1 - dy) + P3[i] * (1 - dx) * dy + P4[i] * dx * dy;
-        Q[i] = input.at<cv::Vec3d>(row , col) [i];
+      Q = &output.at<cv::Vec3b>(i, j);
+      for (int i = 0; i < channels; ++i) {
+        (*Q)[i] = P1[i] * (1 - dx) * (1 - dy) + P2[i] * dx * (1 - dy) + P3[i] * (1 - dx) * dy + P4[i] * dx * dy;
+        // Q[i] = input.at<cv::Vec3b>(i%input_rows, j%input_cols)[i];
       }
     }
   }
--- src/resize/test.cc
+++ src/resize/test.cc
@@ -1,26 +1,27 @@
 #include "impls.h"
 
 
-bool test_my_resize() {
-    cv::Mat input = cv::imread("../assets/resize/input.jpg");
-    cv::resize(input, input, cv::Size(input.cols / 3, input.rows / 3));
+bool test_my_resize()
+{
+  cv::Mat input = cv::imread("../assets/resize/input.jpg");
+  cv::resize(input, input, cv::Size(input.cols / 3, input.rows / 3));
 
-    std::vector<float> scales{1.5f, 0.5f};
-    cv::Mat res;
+  std::vector<float> scales{ 1.5f, 0.5f };
+  cv::Mat res;
 
-    for (const auto& s : scales) {
-        std::string title = "scale = " + std::to_string(s);
+  for (const auto &s : scales) {
+    std::string title = "scale = " + std::to_string(s);
 
-        cv::imshow("原图", input);
+    cv::imshow("原图 input", input);
 
-        cv::Mat resized = my_resize(input, s);
-        cv::imshow(title + "(你的答案)", resized);
+    cv::Mat resized = my_resize(input, s);
+    cv::imshow(title + "(你的答案 your answer)", resized);
 
-        cv::resize(input, res, cv::Size((int)(input.cols * s), (int)(input.rows * s)));
-        cv::imshow(title + "(正确答案)", res);
-        cv::waitKey(0);
-        cv::destroyAllWindows();
-    }
+    cv::resize(input, res, cv::Size((int)(input.cols * s), (int)(input.rows * s)));
+    cv::imshow(title + "(正确答案 correct answer)", res);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+  }
 
-    return true;
+  return true;
 }
